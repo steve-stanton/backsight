279d539e6919026830811d284b6a8727af260300
sstanton@btinternet.com <sstanton@btinternet.com@b7867465-1e3a-0410-9163-efa3f577875f>
2007-09-24 12:19:12 +0000

    Handle the File-Import command in environment editor (#1)

--- trunk/Backsight.SqlServer/TableFactory.cs
+++ trunk/Backsight.SqlServer/TableFactory.cs
@@ -18,10 +18,13 @@ using System.Data.SqlClient;
 using System.Windows.Forms;
 using System.Data;
 using System.Collections.Generic;
+using System.Diagnostics;
 
+using Microsoft.SqlServer.Management.Common;
 using Smo=Microsoft.SqlServer.Management.Smo;
 
 using Backsight.Data;
+using Backsight.Environment;
 
 namespace Backsight.SqlServer
 {
@@ -52,100 +55,113 @@ namespace Backsight.SqlServer
             m_Database = db;
         }
 
+        /// <summary>
+        /// Creates a new <c>TableFactory</c> for the database identified via the supplied
+        /// connection string.
+        /// </summary>
+        /// <param name="connectionString">The connection string, identifying the server and
+        /// database that the factory is for.</param>
+        public TableFactory(string connectionString)
+        {
+            SqlConnectionStringBuilder csb = new SqlConnectionStringBuilder(connectionString);
+
+            // For some reason, the following doesn't initialize the Database object completely
+            // (even calling SetDefaultInitFields). But it works as expected if you first connect
+            // using a ServerConnection.
+      
+            //Smo.Server s = new Smo.Server(csb.DataSource);
+            //s.SetDefaultInitFields(typeof(Smo.Table), true);
+            //m_Database = new Smo.Database(s, csb.InitialCatalog);
+
+            ServerConnection sc = new ServerConnection();
+            sc.ServerInstance = csb.DataSource;
+            sc.ConnectTimeout = 5;
+            sc.LoginSecure = csb.IntegratedSecurity;
+            if (!sc.LoginSecure)
+            {
+                sc.Login = csb.UserID;
+                sc.Password = csb.Password;
+            }
+
+            sc.Connect();
+            string dbName = csb.InitialCatalog;
+            m_Database = new Smo.Server(sc).Databases[dbName];
+        }
+
+        /// <summary>
+        /// Creates a new <c>TableFactory</c> for the database that is defined through
+        /// the <see cref="AdapterFactory.ConnectionString"/> property.
+        /// </summary>
+        public TableFactory()
+            : this(AdapterFactory.ConnectionString)
+        {
+        }
+
         #endregion
 
         /// <summary>
-        /// Creates a database table for each DataTable that's part of a BacksightDataSet.
-        /// Does not define any foreign key constraints (after loading, you should call
-        /// CreateForeignKeyConstraints)
+        /// Checks whether the database contains Backsight system tables.
         /// </summary>
-        /// <param name="handler"></param>
-        public void CreateTables(OnTableCreate handler)
+        /// <returns>True if the entity table exists</returns>
+        public bool DoTablesExist()
         {
+            string testTable = new BacksightDataSet().Entity.TableName;
+            return m_Database.Tables.Contains(testTable);
+        }
+
+        /// <summary>
+        /// Creates a database table for each DataTable that's part of a BacksightDataSet,
+        /// inserts initial rows, and defines constraints.
+        /// </summary>
+        /// <param name="logger">Something to display progress messages (not null)</param>
+        public void CreateTables(ILog logger)
+        {
+            if (logger==null)
+                throw new ArgumentNullException();
+
             DropForeignKeyConstraints();
 
             BacksightDataSet ds = new BacksightDataSet();
             foreach (DataTable dt in ds.Tables)
             {
+                logger.LogMessage("CREATE TABLE "+dt.TableName);
                 CreateTable(dt);
             }
 
             // Add simple checks (unfortunately, this info isn't held as part of the
             // generated DataSet, so need to explicitly identify each table involved)
 
-            AddSimpleChecks(ds.Archive, ds.Archive.Checks);
-            AddSimpleChecks(ds.Checkout, ds.Checkout.Checks);
-            AddSimpleChecks(ds.DomainList, ds.DomainList.Checks);
-            AddSimpleChecks(ds.Domain, ds.Domain.Checks);
-            AddSimpleChecks(ds.Entity, ds.Entity.Checks);
-            AddSimpleChecks(ds.EntitySchema, ds.EntitySchema.Checks);
-            AddSimpleChecks(ds.Field, ds.Field.Checks);
-            AddSimpleChecks(ds.Font, ds.Font.Checks);
-            AddSimpleChecks(ds.IdAllocation, ds.IdAllocation.Checks);
-            AddSimpleChecks(ds.IdFree, ds.IdFree.Checks);
-            AddSimpleChecks(ds.IdGroup, ds.IdGroup.Checks);
-            AddSimpleChecks(ds.Job, ds.Job.Checks);
-            AddSimpleChecks(ds.Layer, ds.Layer.Checks);
-            AddSimpleChecks(ds.Map, ds.Map.Checks);
-            AddSimpleChecks(ds.MapUser, ds.MapUser.Checks);
-            AddSimpleChecks(ds.Schema, ds.Schema.Checks);
-            AddSimpleChecks(ds.SchemaField, ds.SchemaField.Checks);
-            AddSimpleChecks(ds.SchemaTemplate, ds.SchemaTemplate.Checks);
-            AddSimpleChecks(ds.Template, ds.Template.Checks);
-            AddSimpleChecks(ds.Theme, ds.Theme.Checks);
+            AddSimpleChecks(logger, ds.Archive, ds.Archive.Checks);
+            AddSimpleChecks(logger, ds.Checkout, ds.Checkout.Checks);
+            AddSimpleChecks(logger, ds.DomainList, ds.DomainList.Checks);
+            AddSimpleChecks(logger, ds.Domain, ds.Domain.Checks);
+            AddSimpleChecks(logger, ds.Entity, ds.Entity.Checks);
+            AddSimpleChecks(logger, ds.EntitySchema, ds.EntitySchema.Checks);
+            AddSimpleChecks(logger, ds.Field, ds.Field.Checks);
+            AddSimpleChecks(logger, ds.Font, ds.Font.Checks);
+            AddSimpleChecks(logger, ds.IdAllocation, ds.IdAllocation.Checks);
+            AddSimpleChecks(logger, ds.IdFree, ds.IdFree.Checks);
+            AddSimpleChecks(logger, ds.IdGroup, ds.IdGroup.Checks);
+            AddSimpleChecks(logger, ds.Job, ds.Job.Checks);
+            AddSimpleChecks(logger, ds.Layer, ds.Layer.Checks);
+            AddSimpleChecks(logger, ds.Map, ds.Map.Checks);
+            AddSimpleChecks(logger, ds.MapUser, ds.MapUser.Checks);
+            AddSimpleChecks(logger, ds.Schema, ds.Schema.Checks);
+            AddSimpleChecks(logger, ds.SchemaField, ds.SchemaField.Checks);
+            AddSimpleChecks(logger, ds.SchemaTemplate, ds.SchemaTemplate.Checks);
+            AddSimpleChecks(logger, ds.Template, ds.Template.Checks);
+            AddSimpleChecks(logger, ds.Theme, ds.Theme.Checks);
 
-            // Insert initial rows
-            /*
-INSERT INTO [dbo].[Archive] ([ArchiveId], [ArchiveTime], [MapId], [Version], [VersionTime], [JobId], [UserId], [PrevArchiveId])
-VALUES (0, GETDATE(), 0, 0, GETDATE(), 0, 0, 0)
-GO
+            // Insert initial rows & save to database
+            logger.LogMessage("Inserting initial rows");
+            ds.AddInitialRows();
+            ds.Save(ConnectionString);
 
-INSERT INTO [dbo].[Entity] (EntityId, Name, IsPoint, IsLine, IsLineTopological, IsPolygon, IsText, FontId, GroupId)
-VALUES (0, ' ', 'n', 'n', 'n', 'n', 'n', 0, 0 )
-GO
-
-INSERT INTO [dbo].[Font] ([FontId], [TypeFace], [PointSize], [IsBold], [IsItalic], [IsUnderline], [FontFile])
-VALUES (0, ' ', 0.0, 'n', 'n', 'n', ' ')
-GO
-
-INSERT INTO [dbo].[IdGroup] (GroupId, Name, LowestId, HighestId, PacketSize, CheckDigit, KeyFormat)
-VALUES (0, ' ', 1, 1, 1, 'n', '')
-GO
-
-INSERT INTO [dbo].[Job] ([JobId], [Name], [IsActive])
-VALUES (0, ' ', 'n')
-GO
-
-INSERT INTO [dbo].[Layer] (LayerId, Name, BaseLayerId, ThemeId, DefaultPointId, DefaultLineId, DefaultPolygonId, DefaultTextId )
-VALUES (0, ' ', 0, 0, 0, 0, 0, 0)
-GO
-
-INSERT INTO [dbo].[Map] ([MapId], [Name], [Version], [VersionTime], [JobId], [UserId], [ArchiveId], [Status], [IsActive])
-VALUES (0, ' ', 0, GETDATE(), 0, 0, 0, ' ', 'n')
-GO
-
-INSERT INTO [dbo].[MapUser] ([UserId], [Name], [IsActive], [CanCheckout])
-VALUES (0, ' ', 'n', 'n')
-GO
-
-INSERT INTO [dbo].[Theme] (ThemeId, Name)
-VALUES (0, ' ')
-GO
-
-INSERT INTO [dbo].[SysId] (LastId) VALUES (0)
-GO
-             */
-            //CreateForeignKeyConstraints();
+            // Define foreign key constraints
+            AddForeignKeyConstraints(logger);
         }
 
-        public void CreateForeignKeyConstraints()
-        {
-            BacksightDataSet ds = new BacksightDataSet();
-            foreach (DataRelation dr in ds.Relations)
-                AddForeignKeyConstraint(dr);
-        }
-
-        void AddSimpleChecks(DataTable dt, string[] checks)
+        void AddSimpleChecks(ILog logger, DataTable dt, string[] checks)
         {
             Smo.Table t = m_Database.Tables[dt.TableName];
             if (t==null)
@@ -154,7 +170,30 @@ GO
             for (int i=0; i<checks.Length; i++)
             {
                 string checkName = String.Format("{0}Check{1}", t.Name, i+1);
+                logger.LogMessage("ADD CHECK "+checkName);
                 AddCheck(t, checkName, checks[i]);
+            }
+        }
+
+        void AddForeignKeyConstraints(ILog logger)
+        {
+            BacksightDataSet ds = new BacksightDataSet();
+
+            foreach (DataRelation dr in ds.Relations)
+            {
+                DataTable parent = dr.ParentTable;
+                DataTable child = dr.ChildTable;
+
+                Smo.Table parentTable = m_Database.Tables[parent.TableName];
+                Smo.Table childTable = m_Database.Tables[child.TableName];
+
+                if (parentTable!=null && childTable!=null)
+                {
+                    if (logger!=null)
+                        logger.LogMessage("ADD CONSTRAINT "+dr.RelationName);
+
+                    AddForeignKeyConstraint(dr);
+                }
             }
         }
 
@@ -328,6 +367,9 @@ GO
             t.Alter();
         }
 
+        /// <summary>
+        /// The connection string for the database this factory is for.
+        /// </summary>
         public string ConnectionString
         {
             get
@@ -337,10 +379,99 @@ GO
             }
         }
 
-        public SqlConnection CreateConnection()
+        /// <summary>
+        /// Enable or disable foreign key constraints for Backsight system tables.
+        /// </summary>
+        /// <param name="enable">Specify <c>false</c> to enable constraints, <c>true</c>
+        /// to re-enable.</param>
+        public void EnableForeignKeys(bool enable)
         {
-            string constr = this.ConnectionString;
-            return new SqlConnection(constr);
+            // Do it the heavy-handed way, since I'm still getting foreign key errors
+            // after setting ForeignKey.IsEnabled to false.
+
+            if (enable)
+                AddForeignKeyConstraints(null);
+            else
+                DropForeignKeyConstraints();
+
+            /*
+            BacksightDataSet ds = new BacksightDataSet();
+
+            foreach (DataTable dt in ds.Tables)
+            {
+                Smo.Table t = m_Database.Tables[dt.TableName];
+                if (t!=null)
+                {
+                    foreach (Smo.ForeignKey fk in t.ForeignKeys)
+                        fk.IsEnabled = enable;
+                }
+            }
+            */
+        }
+
+        /// <summary>
+        /// Removes all data from all Backsight tables. This is done when a new environment
+        /// is being imported. You will probably need to bracket this call with calls
+        /// to <see cref="EnableForeignKeys"/> (disable foreign key constraints, then
+        /// remove, then import, then re-enable constraints).
+        /// </summary>
+        public void RemoveAll()
+        {
+            BacksightDataSet ds = new BacksightDataSet();
+
+            using (IConnection ic = AdapterFactory.GetConnection())
+            {
+                SqlConnection c = ic.Value;
+
+                foreach (DataTable dt in ds.Tables)
+                {
+                    // Not sure why, but it complains if you don't specify the table schema
+                    // at this point. Assume "dbo", since I don't see any DataTable.SchemaName
+                    // property.
+                    string sql = String.Format("DELETE FROM [{0}].[{1}]", "dbo", dt.TableName);
+                    SqlCommand cmd = new SqlCommand(sql, c);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Imports environment data from the specified dataset. Prior to call, you should
+        /// first call <see cref="RemoveAll"/>.
+        /// </summary>
+        /// <param name="ds">The dataset to import</param>
+        public void Import(BacksightDataSet ds)
+        {
+            using (IConnection ic = AdapterFactory.GetConnection())
+            {
+                SqlConnection c = ic.Value;
+
+                foreach (DataTable dt in ds.Tables)
+                {
+                    if (dt.Rows.Count > 0)
+                    {
+                        SqlBulkCopy bc = new SqlBulkCopy(c);
+                        bc.DestinationTableName = dt.TableName;
+                        bc.BatchSize = dt.Rows.Count;
+                        bc.WriteToServer(dt);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns the names of all tables in the database this factory is associated with.
+        /// </summary>
+        /// <returns>The names of all tables in the database</returns>
+        public string[] GetTableNames()
+        {
+            Smo.TableCollection tc = m_Database.Tables;
+            List<string> names = new List<string>(tc.Count);
+
+            foreach (Smo.Table t in tc)
+                names.Add(t.Name);
+
+            return names.ToArray();
         }
     }
 }
