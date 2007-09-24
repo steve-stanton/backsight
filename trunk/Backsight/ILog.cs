130768cbc16d0f1033057d1eefe86a7a416bd2af
sstanton@btinternet.com <sstanton@btinternet.com@b7867465-1e3a-0410-9163-efa3f577875f>
2007-09-24 08:49:13 +0000

    Add ILog interface

---
+++ trunk/Backsight/ILog.cs
@@ -0,0 +1,33 @@
+/// <remarks>
+/// Copyright 2007 - Steve Stanton. This file is part of Foresight
+///
+/// Foresight is free software; you can redistribute it and/or modify it under the terms
+/// of the GNU Lesser General Public License as published by the Free Software Foundation;
+/// either version 3 of the License, or (at your option) any later version.
+///
+/// Foresight is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+/// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+/// See the GNU Lesser General Public License for more details.
+///
+/// You should have received a copy of the GNU Lesser General Public License
+/// along with this program. If not, see <http://www.gnu.org/licenses/>.
+/// </remarks>
+
+using System;
+
+namespace Backsight
+{
+    /// <written by="Steve Stanton" on="20-SEP-2007" />
+    /// <summary>
+    /// Something that logs messages (might just display them on a screen, might stuff
+    /// them into some archive, whatever).
+    /// </summary>
+    public interface ILog
+    {
+        /// <summary>
+        /// Logs the specified message
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        void LogMessage(string message);
+    }
+}
