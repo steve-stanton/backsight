0894ffc082e465daaeaf7b29cf88d2c08aa181b6
sstanton@btinternet.com <sstanton@btinternet.com@b7867465-1e3a-0410-9163-efa3f577875f>
2008-02-15 14:32:07 +0000

    Start View - AttributeStructure command

---
+++ trunk/Backsight.Editor/Forms/EnvironmentStructureForm.Designer.cs
@@ -0,0 +1,131 @@
+namespace Backsight.Editor.Forms
+{
+    partial class EnvironmentStructureForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EnvironmentStructureForm));
+            this.tree = new System.Windows.Forms.TreeView();
+            this.okButton = new System.Windows.Forms.Button();
+            this.colorLabel = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tree
+            // 
+            this.tree.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tree.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tree.ImageIndex = 0;
+            this.tree.ImageList = this.imageList;
+            this.tree.Location = new System.Drawing.Point(0, 0);
+            this.tree.Name = "tree";
+            this.tree.SelectedImageIndex = 0;
+            this.tree.Size = new System.Drawing.Size(422, 309);
+            this.tree.TabIndex = 0;
+            // 
+            // okButton
+            // 
+            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.okButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.okButton.Location = new System.Drawing.Point(12, 15);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 1;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // colorLabel
+            // 
+            this.colorLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.colorLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.colorLabel.Location = new System.Drawing.Point(12, 267);
+            this.colorLabel.Name = "colorLabel";
+            this.colorLabel.Size = new System.Drawing.Size(75, 31);
+            this.colorLabel.TabIndex = 2;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tree);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.okButton);
+            this.splitContainer1.Panel2.Controls.Add(this.colorLabel);
+            this.splitContainer1.Size = new System.Drawing.Size(526, 309);
+            this.splitContainer1.SplitterDistance = 422;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // imageList
+            // 
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "AttributeContainer");
+            this.imageList.Images.SetKeyName(1, "Theme");
+            this.imageList.Images.SetKeyName(2, "Body");
+            this.imageList.Images.SetKeyName(3, "TableDefinition");
+            this.imageList.Images.SetKeyName(4, "FieldDefinition");
+            this.imageList.Images.SetKeyName(5, "Domain");
+            this.imageList.Images.SetKeyName(6, "Template");
+            this.imageList.Images.SetKeyName(7, "Layer");
+            // 
+            // EnvironmentStructureForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 309);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "EnvironmentStructureForm";
+            this.Text = "Attribute Structure";
+            this.Shown += new System.EventHandler(this.EnvironmentStructureForm_Shown);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView tree;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Label colorLabel;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ImageList imageList;
+    }
+}
