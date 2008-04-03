5e9d7c466b9da8d89fca6b5000e216e744b69f3f
sstanton@btinternet.com <sstanton@btinternet.com@b7867465-1e3a-0410-9163-efa3f577875f>
2008-04-03 08:43:29 +0000

    Conversion of Add Miscellaneous Text command

--- trunk/Backsight.Editor/Forms/NewTextForm.cs
+++ trunk/Backsight.Editor/Forms/NewTextForm.cs
@@ -27,25 +27,116 @@ namespace Backsight.Editor.Forms
     {
         #region Class data
 
+        /// <summary>
+        /// The label being updated (if it's an update).
+        /// </summary>
+        readonly TextFeature m_Label;
+
+        /// <summary>
+        /// The text specified by the user
+        /// </summary>
+        string m_Text;
+
+        /// <summary>
+        /// The entity type for the text.
+        /// </summary>
+        IEntity m_Entity;
+
         #endregion
 
         #region Constructors
 
-        internal NewTextForm()
+        /// <summary>
+        /// Creates a new <c>NewTextForm</c>
+        /// </summary>
+        /// <param name="label">Optional label that's being updated (null if
+        /// specifying a new item of text) [default was null]</param>
+        internal NewTextForm(TextFeature label)
         {
             InitializeComponent();
+
+            m_Text = String.Empty;
+            m_Entity = null;
+            m_Label = label;
+
+            // If we're doing an update, grab initial values.
+            if (m_Label!=null)
+            {
+                m_Text = m_Label.TextGeometry.Text;
+                m_Entity = m_Label.EntityType;
+            }
         }
 
         #endregion
 
-        internal string GetText()
+        /// <summary>
+        /// The text specified by the user
+        /// </summary>
+        internal string EnteredText // was GetText()
         {
-            return null;
+            get { return m_Text; }
         }
 
+        /// <summary>
+        /// The entity type for the text.
+        /// </summary>
         internal IEntity EntityType
         {
             get { return null; }
+        }
+
+        private void NewTextForm_Shown(object sender, EventArgs e)
+        {
+            // If we already have an initial string, display it.
+            textTextBox.Text = m_Text;
+
+            // Load up entity combo box with the default type for misc text.
+            entityTypeComboBox.Load(SpatialType.Text);
+
+            // If we are doing an update, scroll to the entity type we
+            // already have, and alter the window title.
+            if (m_Entity!=null)
+            {
+                entityTypeComboBox.SelectEntity(m_Entity);
+                this.Text = "Update Miscellaneous Text";
+            }
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            // The entity type must be defined.
+            m_Entity = entityTypeComboBox.SelectedEntityType;
+            if (m_Entity==null)
+            {
+                MessageBox.Show("The text type hasn't been specified.");
+                entityTypeComboBox.Focus();
+                return;
+            }
+
+            // The text must be defined (obviously)
+            m_Text = textTextBox.Text.Trim();
+            if (m_Text.Length==0)
+            {
+                MessageBox.Show("You haven't specified any text");
+                textTextBox.Focus();
+                return;
+            }
+
+            // If the specified entity type is not the default for misc
+            // text, make it the default.
+            CadastralMapModel map = CadastralMapModel.Current;
+            IEntity curDef = map.DefaultTextType;
+            if (curDef==null || curDef.Id!=m_Entity.Id)
+                map.DefaultTextType = m_Entity;
+
+            DialogResult = DialogResult.OK;
+            Close();
         }
     }
 }
