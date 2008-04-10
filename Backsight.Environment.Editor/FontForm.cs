4ba0da3b81729c32ecb9f1b304a6008eadef2bec
sstanton <sstanton@btinternet.com>
2008-04-10 13:26:46 +0000

    Provide font dialog for environment editor

--- Backsight.Environment.Editor/FontForm.cs
+++ Backsight.Environment.Editor/FontForm.cs
@@ -15,6 +15,7 @@
 
 using System;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace Backsight.Environment.Editor
 {
@@ -42,7 +43,48 @@ namespace Backsight.Environment.Editor
 
         private void FontForm_Shown(object sender, EventArgs e)
         {
-            IEnvironmentContainer ec = EnvironmentContainer.Current;
+            FontFamily[] fams = FontFamily.Families;
+            fontFamilyComboBox.DataSource = fams;
+            fontFamilyComboBox.DisplayMember = "Name";
+
+            fontFamilyComboBox.SelectedIndex = 0;
+            fontStyleComboBox.SelectedIndex = 0;
+            sizeComboBox.SelectedIndex = 0;
+
+            fontFamilyComboBox.Text = fontFamilyComboBox.SelectedText;
+            fontStyleComboBox.Text = fontStyleComboBox.SelectedText;
+            sizeComboBox.Text = sizeComboBox.SelectedText;
+
+            if (m_Edit.Id!=0)
+            {
+                fontFamilyComboBox.SelectedItem = Array.Find<FontFamily>(fams,
+                    delegate(FontFamily ff) { return ff.Name==m_Edit.TypeFace; });
+
+                bool isBold = (m_Edit.Modifiers & FontStyle.Bold)!=0;
+                bool isItalic = (m_Edit.Modifiers & FontStyle.Italic)!=0;
+
+                if (isBold && isItalic)
+                    fontStyleComboBox.SelectedItem = "Bold Italic";
+                else if (isBold)
+                    fontStyleComboBox.SelectedItem = "Bold";
+                else if (isItalic)
+                    fontStyleComboBox.SelectedItem = "Italic";
+                else
+                    fontStyleComboBox.SelectedItem = "Regular";
+
+                string s = m_Edit.PointSize.ToString();
+                if (sizeComboBox.Items.Contains(s))
+                    sizeComboBox.SelectedItem = s;
+                else
+                {
+                    sizeComboBox.SelectedItem = null;
+                    sizeComboBox.Text = s;
+                }
+            }
+
+            fontFamilyComboBox.Text = fontFamilyComboBox.SelectedText;
+            fontStyleComboBox.Text = fontStyleComboBox.SelectedText;
+            sizeComboBox.Text = sizeComboBox.SelectedText;
         }
 
         private void cancelButton_Click(object sender, EventArgs e)
@@ -54,7 +96,25 @@ namespace Backsight.Environment.Editor
 
         private void okButton_Click(object sender, EventArgs e)
         {
-            // TODO: make changes
+            m_Edit.TypeFace = fontFamilyComboBox.Text;
+
+            string fs = fontStyleComboBox.Text;
+            if (fs == "Regular")
+                m_Edit.Modifiers = FontStyle.Regular;
+            else if (fs == "Italic")
+                m_Edit.Modifiers = FontStyle.Italic;
+            else if (fs == "Bold")
+                m_Edit.Modifiers = FontStyle.Bold;
+            else if (fs == "Bold Italic")
+                m_Edit.Modifiers = (FontStyle.Bold | FontStyle.Italic);
+
+            float size;
+            if (!Single.TryParse(sizeComboBox.Text, out size))
+            {
+                MessageBox.Show("Cannot parse font size");
+                return;
+            }
+            m_Edit.PointSize = size;
 
             m_Edit.FinishEdit();
             this.DialogResult = DialogResult.OK;
