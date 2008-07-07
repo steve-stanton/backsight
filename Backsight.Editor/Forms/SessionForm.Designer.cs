56be069228c6b0f82400d0471377b8ce116f1ae8
sstanton <sstanton@btinternet.com>
2008-07-07 12:58:44 +0000

    Provide access to XML details from edit history

--- Backsight.Editor/Forms/SessionForm.Designer.cs
+++ Backsight.Editor/Forms/SessionForm.Designer.cs
@@ -34,6 +34,7 @@ namespace Backsight.Editor.Forms
             this.EditSequence = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.EditType = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.FeatureCount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.detailsButton = new System.Windows.Forms.Button();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
@@ -44,7 +45,7 @@ namespace Backsight.Editor.Forms
             // 
             this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.closeButton.Location = new System.Drawing.Point(195, 11);
+            this.closeButton.Location = new System.Drawing.Point(373, 11);
             this.closeButton.Name = "closeButton";
             this.closeButton.Size = new System.Drawing.Size(75, 23);
             this.closeButton.TabIndex = 0;
@@ -66,6 +67,7 @@ namespace Backsight.Editor.Forms
             // 
             // splitContainer1.Panel2
             // 
+            this.splitContainer1.Panel2.Controls.Add(this.detailsButton);
             this.splitContainer1.Panel2.Controls.Add(this.closeButton);
             this.splitContainer1.Size = new System.Drawing.Size(460, 262);
             this.splitContainer1.SplitterDistance = 212;
@@ -87,6 +89,7 @@ namespace Backsight.Editor.Forms
             this.grid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.grid.Size = new System.Drawing.Size(460, 212);
             this.grid.TabIndex = 0;
+            this.grid.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grid_CellDoubleClick);
             this.grid.SelectionChanged += new System.EventHandler(this.grid_SelectionChanged);
             // 
             // EditSequence
@@ -115,6 +118,18 @@ namespace Backsight.Editor.Forms
             this.FeatureCount.Name = "FeatureCount";
             this.FeatureCount.ReadOnly = true;
             // 
+            // detailsButton
+            // 
+            this.detailsButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.detailsButton.Location = new System.Drawing.Point(12, 11);
+            this.detailsButton.Name = "detailsButton";
+            this.detailsButton.Size = new System.Drawing.Size(75, 23);
+            this.detailsButton.TabIndex = 1;
+            this.detailsButton.Text = "&Details";
+            this.detailsButton.UseVisualStyleBackColor = true;
+            this.detailsButton.Click += new System.EventHandler(this.detailsButton_Click);
+            // 
             // SessionForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -141,5 +156,6 @@ namespace Backsight.Editor.Forms
         private System.Windows.Forms.DataGridViewTextBoxColumn EditSequence;
         private System.Windows.Forms.DataGridViewTextBoxColumn EditType;
         private System.Windows.Forms.DataGridViewTextBoxColumn FeatureCount;
+        private System.Windows.Forms.Button detailsButton;
     }
 }
