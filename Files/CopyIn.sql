81881495c72471db8ba881077f50edebd612a8c8
sstanton <sstanton@btinternet.com>
2009-02-05 16:45:21 +0000

    Load sample attribute data

--- Files/CopyIn.sql
+++ Files/CopyIn.sql
@@ -1,5 +1,5 @@
 BULK INSERT [dbo].[PlanParcelData] FROM 'C:\Temp\A10.txt'
-WITH (DATAFILETYPE='char');
+WITH (DATAFILETYPE='char', FORMATFILE='PlanParcelData.xml', MAXERRORS=0);
 GO
 
 bcp Backsight.dbo.PlanParcelData in A10.txt -f PlanParcelData.xml -T -S localhost\sqlexpress
