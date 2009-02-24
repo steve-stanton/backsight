bc2ddf362aaf2c1201676a4950968b09b41735a9
sstanton@btinternet.com <sstanton@btinternet.com@b7867465-1e3a-0410-9163-efa3f577875f>
2009-02-24 16:23:51 +0000

    Improve attribute data entry dialog

--- trunk/Backsight.Editor/Forms/AttributeDataForm.Designer.cs
+++ trunk/Backsight.Editor/Forms/AttributeDataForm.Designer.cs
@@ -28,27 +28,29 @@ namespace Backsight.Editor.Forms
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.splitContainer = new System.Windows.Forms.SplitContainer();
-            this.domainGrid = new System.Windows.Forms.DataGridView();
+            this.gridContainer = new System.Windows.Forms.SplitContainer();
             this.grid = new System.Windows.Forms.DataGridView();
-            this.okButton = new System.Windows.Forms.Button();
-            this.cancelButton = new System.Windows.Forms.Button();
             this.dgcColumnName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dgcValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.gridContainer = new System.Windows.Forms.SplitContainer();
+            this.domainGrid = new System.Windows.Forms.DataGridView();
             this.dgcShortValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dgcLongValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.splitContainer.Panel1.SuspendLayout();
             this.splitContainer.Panel2.SuspendLayout();
             this.splitContainer.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.domainGrid)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
             this.gridContainer.Panel1.SuspendLayout();
             this.gridContainer.Panel2.SuspendLayout();
             this.gridContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.domainGrid)).BeginInit();
             this.SuspendLayout();
             // 
             // splitContainer
@@ -67,15 +69,73 @@ namespace Backsight.Editor.Forms
             // 
             this.splitContainer.Panel2.Controls.Add(this.okButton);
             this.splitContainer.Panel2.Controls.Add(this.cancelButton);
-            this.splitContainer.Size = new System.Drawing.Size(645, 579);
-            this.splitContainer.SplitterDistance = 507;
+            this.splitContainer.Size = new System.Drawing.Size(691, 591);
+            this.splitContainer.SplitterDistance = 517;
             this.splitContainer.TabIndex = 0;
+            // 
+            // gridContainer
+            // 
+            this.gridContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridContainer.Location = new System.Drawing.Point(0, 0);
+            this.gridContainer.Margin = new System.Windows.Forms.Padding(10);
+            this.gridContainer.Name = "gridContainer";
+            // 
+            // gridContainer.Panel1
+            // 
+            this.gridContainer.Panel1.Controls.Add(this.label2);
+            this.gridContainer.Panel1.Controls.Add(this.grid);
+            // 
+            // gridContainer.Panel2
+            // 
+            this.gridContainer.Panel2.Controls.Add(this.label1);
+            this.gridContainer.Panel2.Controls.Add(this.domainGrid);
+            this.gridContainer.Size = new System.Drawing.Size(691, 517);
+            this.gridContainer.SplitterDistance = 368;
+            this.gridContainer.TabIndex = 3;
+            // 
+            // grid
+            // 
+            this.grid.AllowUserToAddRows = false;
+            this.grid.AllowUserToDeleteRows = false;
+            this.grid.AllowUserToResizeRows = false;
+            this.grid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grid.ColumnHeadersHeight = 24;
+            this.grid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgcColumnName,
+            this.dgcValue});
+            this.grid.Location = new System.Drawing.Point(12, 47);
+            this.grid.MultiSelect = false;
+            this.grid.Name = "grid";
+            this.grid.RowHeadersVisible = false;
+            this.grid.Size = new System.Drawing.Size(347, 447);
+            this.grid.TabIndex = 1;
+            this.grid.SelectionChanged += new System.EventHandler(this.grid_SelectionChanged);
+            // 
+            // dgcColumnName
+            // 
+            this.dgcColumnName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.dgcColumnName.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dgcColumnName.HeaderText = "Column Name";
+            this.dgcColumnName.Name = "dgcColumnName";
+            this.dgcColumnName.ReadOnly = true;
+            // 
+            // dgcValue
+            // 
+            this.dgcValue.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dgcValue.HeaderText = "Value";
+            this.dgcValue.Name = "dgcValue";
             // 
             // domainGrid
             // 
             this.domainGrid.AllowUserToAddRows = false;
             this.domainGrid.AllowUserToDeleteRows = false;
             this.domainGrid.AllowUserToResizeRows = false;
+            this.domainGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.domainGrid.BackgroundColor = System.Drawing.Color.Moccasin;
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle2.BackColor = System.Drawing.Color.Moccasin;
@@ -97,89 +157,16 @@ namespace Backsight.Editor.Forms
             dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.domainGrid.DefaultCellStyle = dataGridViewCellStyle3;
-            this.domainGrid.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.domainGrid.Location = new System.Drawing.Point(0, 0);
+            this.domainGrid.Location = new System.Drawing.Point(20, 47);
             this.domainGrid.Margin = new System.Windows.Forms.Padding(10);
             this.domainGrid.MultiSelect = false;
             this.domainGrid.Name = "domainGrid";
             this.domainGrid.ReadOnly = true;
             this.domainGrid.RowHeadersVisible = false;
             this.domainGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.domainGrid.Size = new System.Drawing.Size(297, 507);
+            this.domainGrid.Size = new System.Drawing.Size(287, 447);
             this.domainGrid.TabIndex = 2;
             this.domainGrid.SelectionChanged += new System.EventHandler(this.domainGrid_SelectionChanged);
-            // 
-            // grid
-            // 
-            this.grid.AllowUserToAddRows = false;
-            this.grid.AllowUserToDeleteRows = false;
-            this.grid.AllowUserToResizeRows = false;
-            this.grid.ColumnHeadersHeight = 24;
-            this.grid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.dgcColumnName,
-            this.dgcValue});
-            this.grid.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.grid.Location = new System.Drawing.Point(0, 0);
-            this.grid.MultiSelect = false;
-            this.grid.Name = "grid";
-            this.grid.RowHeadersVisible = false;
-            this.grid.Size = new System.Drawing.Size(344, 507);
-            this.grid.TabIndex = 1;
-            this.grid.SelectionChanged += new System.EventHandler(this.grid_SelectionChanged);
-            // 
-            // okButton
-            // 
-            this.okButton.Location = new System.Drawing.Point(558, 23);
-            this.okButton.Name = "okButton";
-            this.okButton.Size = new System.Drawing.Size(75, 23);
-            this.okButton.TabIndex = 1;
-            this.okButton.Text = "OK";
-            this.okButton.UseVisualStyleBackColor = true;
-            this.okButton.Click += new System.EventHandler(this.okButton_Click);
-            // 
-            // cancelButton
-            // 
-            this.cancelButton.Location = new System.Drawing.Point(468, 23);
-            this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(75, 23);
-            this.cancelButton.TabIndex = 0;
-            this.cancelButton.TabStop = false;
-            this.cancelButton.Text = "Cancel";
-            this.cancelButton.UseVisualStyleBackColor = true;
-            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
-            // 
-            // dgcColumnName
-            // 
-            this.dgcColumnName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.dgcColumnName.DefaultCellStyle = dataGridViewCellStyle1;
-            this.dgcColumnName.HeaderText = "Column Name";
-            this.dgcColumnName.Name = "dgcColumnName";
-            this.dgcColumnName.ReadOnly = true;
-            // 
-            // dgcValue
-            // 
-            this.dgcValue.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.dgcValue.HeaderText = "Value";
-            this.dgcValue.Name = "dgcValue";
-            // 
-            // gridContainer
-            // 
-            this.gridContainer.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.gridContainer.Location = new System.Drawing.Point(0, 0);
-            this.gridContainer.Margin = new System.Windows.Forms.Padding(10);
-            this.gridContainer.Name = "gridContainer";
-            // 
-            // gridContainer.Panel1
-            // 
-            this.gridContainer.Panel1.Controls.Add(this.grid);
-            // 
-            // gridContainer.Panel2
-            // 
-            this.gridContainer.Panel2.Controls.Add(this.domainGrid);
-            this.gridContainer.Size = new System.Drawing.Size(645, 507);
-            this.gridContainer.SplitterDistance = 344;
-            this.gridContainer.TabIndex = 3;
             // 
             // dgcShortValue
             // 
@@ -196,12 +183,55 @@ namespace Backsight.Editor.Forms
             this.dgcLongValue.Name = "dgcLongValue";
             this.dgcLongValue.ReadOnly = true;
             // 
+            // okButton
+            // 
+            this.okButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.okButton.Location = new System.Drawing.Point(357, 21);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 1;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.cancelButton.Location = new System.Drawing.Point(267, 21);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 0;
+            this.cancelButton.TabStop = false;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Domain values";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Data values";
+            // 
             // AttributeDataForm
             // 
             this.AcceptButton = this.okButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(645, 579);
+            this.ClientSize = new System.Drawing.Size(691, 591);
             this.Controls.Add(this.splitContainer);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "AttributeDataForm";
@@ -210,11 +240,13 @@ namespace Backsight.Editor.Forms
             this.splitContainer.Panel1.ResumeLayout(false);
             this.splitContainer.Panel2.ResumeLayout(false);
             this.splitContainer.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.domainGrid)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
             this.gridContainer.Panel1.ResumeLayout(false);
+            this.gridContainer.Panel1.PerformLayout();
             this.gridContainer.Panel2.ResumeLayout(false);
+            this.gridContainer.Panel2.PerformLayout();
             this.gridContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.domainGrid)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -231,5 +263,7 @@ namespace Backsight.Editor.Forms
         private System.Windows.Forms.SplitContainer gridContainer;
         private System.Windows.Forms.DataGridViewTextBoxColumn dgcShortValue;
         private System.Windows.Forms.DataGridViewTextBoxColumn dgcLongValue;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
