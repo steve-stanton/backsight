f91bf4f32f790cb204667938653754a582f6c358
sstanton <sstanton@btinternet.com>
2009-03-12 23:12:16 +0000

    Experiment with serialization code

--- Backsight.Editor/TextGeometry.cs
+++ Backsight.Editor/TextGeometry.cs
@@ -385,15 +385,6 @@ namespace Backsight.Editor
         }
 
         /// <summary>
-        /// Writes any child elements of this class. This will be called after
-        /// all attributes have been written via <see cref="WriteAttributes"/>.
-        /// </summary>
-        /// <param name="writer">The writing tool</param>
-        public override void WriteChildElements(XmlContentWriter writer)
-        {
-        }
-
-        /// <summary>
         /// Defines the attributes of this content
         /// </summary>
         /// <param name="reader">The reading tool</param>
@@ -410,15 +401,6 @@ namespace Backsight.Editor
                 m_Rotation = new RadianValue(rot);
             else
                 throw new Exception("Cannot parse text rotation");
-        }
-
-        /// <summary>
-        /// Defines any child content related to this instance. This will be called after
-        /// all attributes have been defined via <see cref="ReadAttributes"/>.
-        /// </summary>
-        /// <param name="reader">The reading tool</param>
-        public override void ReadChildElements(XmlContentReader reader)
-        {
         }
     }
 }
