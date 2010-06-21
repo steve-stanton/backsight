3f7fb9c9221ec060b39be57578d669a94a85d5c2
sstanton@btinternet.com <sstanton@btinternet.com@b7867465-1e3a-0410-9163-efa3f577875f>
2010-06-21 17:07:52 +0000

    Experiment with FeatureStub

---
+++ trunk/Backsight.Editor/FeatureGeometry.cs
@@ -0,0 +1,72 @@
+﻿// <remarks>
+// Copyright 2010 - Steve Stanton. This file is part of Backsight
+//
+// Backsight is free software; you can redistribute it and/or modify it under the terms
+// of the GNU Lesser General Public License as published by the Free Software Foundation;
+// either version 3 of the License, or (at your option) any later version.
+//
+// Backsight is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+// </remarks>
+
+namespace Backsight.Editor
+{
+    /// <summary>
+    /// Identifies a type of geometry for spatial features (for use as part of the
+    /// <see cref="FeatureStub"/> class).
+    /// </summary>
+    enum FeatureGeometry
+    {
+        /// <summary>
+        /// A point with an explicit position
+        /// (corresponding to the <see cref="DirectPointFeature"/> class)
+        /// </summary>
+        DirectPoint = 10,
+
+        /// <summary>
+        /// A point that shares the same position as another point
+        /// (corresponding to the <see cref="SharedPointFeature"/> class)
+        /// </summary>
+        SharedPoint = 11,
+
+        /// <summary>
+        /// A simple line segment, consisting of two positions
+        /// (corresponding to the <see cref="SegmentLineFeature"/> class).
+        /// </summary>
+        Segment = 20,
+
+        /// <summary>
+        /// A circular arc
+        /// (corresponding to the <see cref="ArcFeature"/> class).
+        /// </summary>
+        Arc = 21,
+
+        /// <summary>
+        /// A line containing 3 or more explicit positions
+        /// (corresponding to the <see cref="MultiSegmentLineFeature"/> class).
+        /// </summary>
+        MultiSegment = 22,
+
+        /// <summary>
+        /// A text label that portrays the user-perceived ID for a feature
+        /// (corresponding to the <see cref="KeyTextFeature"/> class).
+        /// </summary>
+        KeyText = 30,
+
+        /// <summary>
+        /// A text label that portrays database attributes of a feature.
+        /// (corresponding to the <see cref="RowTextFeature"/> class).
+        /// </summary>
+        RowText=31,
+
+        /// <summary>
+        /// A text label that portrays a miscellaneous item of text
+        /// (corresponding to the <see cref="MiscTextFeature"/> class).
+        /// </summary>
+        MiscText=32,
+    }
+}
---
+++ trunk/Backsight.Editor/FeatureStub.cs
@@ -0,0 +1,55 @@
+﻿// <remarks>
+// Copyright 2010 - Steve Stanton. This file is part of Backsight
+//
+// Backsight is free software; you can redistribute it and/or modify it under the terms
+// of the GNU Lesser General Public License as published by the Free Software Foundation;
+// either version 3 of the License, or (at your option) any later version.
+//
+// Backsight is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+// </remarks>
+
+using Backsight.Environment;
+
+namespace Backsight.Editor
+{
+    /// <summary>
+    /// A placeholder for a spatial feature, used during deserialization from the database.
+    /// </summary>
+    class FeatureStub : Feature
+    {
+        #region Class data
+
+        /// <summary>
+        /// The type of geometry that is used for the feature (when converted from a stub).
+        /// </summary>
+        readonly FeatureGeometry m_GeometricType;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FeatureStub"/> class
+        /// </summary>
+        /// <param name="iid">The internal ID for the feature.</param>
+        /// <param name="fid">The (optional) user-perceived ID for the feature.</param>
+        /// <param name="ent">The entity type for the feature (not null)</param>
+        /// <param name="creator">The operation creating the feature (not null). Expected to
+        /// refer to an editing session that is consistent with the session ID that is part
+        /// of the feature's internal ID.</param>
+        /// <exception cref="ArgumentNullException">If either <paramref name="ent"/> or
+        /// <paramref name="creator"/> is null.</exception>
+        internal FeatureStub(InternalIdValue iid, FeatureId fid, IEntity ent, Operation creator, FeatureGeometry geometricType)
+            : base(iid, fid, ent, creator)
+        {
+            m_GeometricType = geometricType;
+        }
+
+        #endregion
+    }
+}
