9a1329c25986b382cccf9f37b44c165bde6a8f04
sstanton <sstanton@btinternet.com>
2010-07-19 23:32:30 +0000

    Remove unused usings (A-E in Backsight.Editor)

--- Backsight.Editor/CheckItem.cs
+++ Backsight.Editor/CheckItem.cs
@@ -13,7 +13,6 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 // </remarks>
 
-using System;
 using System.Text;
 using System.Drawing;
 using System.Collections.Generic;
