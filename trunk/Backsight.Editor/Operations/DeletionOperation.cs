3dd7173e9ac2cf4829831a9921529ae515f6c9d1
sstanton@btinternet.com <sstanton@btinternet.com@b7867465-1e3a-0410-9163-efa3f577875f>
2010-09-13 12:40:44 +0000

    Fix misleading comment

--- trunk/Backsight.Editor/Operations/DeletionOperation.cs
+++ trunk/Backsight.Editor/Operations/DeletionOperation.cs
@@ -45,7 +45,8 @@ namespace Backsight.Editor.Operations
         /// Initializes a new instance of the <see cref="DeletionOperation"/> class
         /// that refers to nothing.
         /// </summary>
-        /// <param name="s">The session the new instance should be added to</param>
+        /// <param name="s">The session the operation should be referred to (the session itself
+        /// is not modified until the editing operation is saved to the database).</param>
         /// <param name="sequence">The sequence number of the edit within the session (specify 0 if
         /// a new sequence number should be reserved). A non-zero value is specified during
         /// deserialization from the database.</param>
--- trunk/Backsight.Editor/Operations/UpdateOperation.cs
+++ trunk/Backsight.Editor/Operations/UpdateOperation.cs
@@ -36,15 +36,35 @@ namespace Backsight.Editor.Operations
         /// <summary>
         /// Information about the update (not null)
         /// </summary>
-        //readonly UpdateData m_Update;
+        readonly UpdateItem[] m_Changes;
 
         #endregion
 
         #region Constructors
 
-        internal UpdateOperation(Session s, uint sequence)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UpdateOperation"/> class.
+        /// </summary>
+        /// <param name="s">The session the operation should be referred to (the session itself
+        /// is not modified until the editing operation is saved to the database).</param>
+        /// <param name="sequence">The sequence number of the edit within the session (specify 0 if
+        /// a new sequence number should be reserved). A non-zero value is specified during
+        /// deserialization from the database.</param>
+        /// <exception cref="ArgumentNullException">If either <paramref name="edit"/> or
+        /// <paramref name="changes"/> is null.</exception>
+        /// <exception cref="ArgumentException">If <paramref name="changes"/> is an
+        /// empty array.</exception>
+        internal UpdateOperation(Session s, uint sequence, Operation edit, UpdateItem[] changes)
             : base(s, sequence)
         {
+            if (edit == null || changes == null)
+                throw new ArgumentNullException();
+
+            if (changes.Length == 0)
+                throw new ArgumentException("Empty change list");
+
+            m_Edit = edit;
+            m_Changes = changes;
         }
 
         #endregion
@@ -57,16 +77,30 @@ namespace Backsight.Editor.Operations
             get { return m_Edit.Name + " (update)"; }
         }
 
+        /// <summary>
+        /// Finds the observed length of a line that was created by this operation.
+        /// </summary>
+        /// <param name="line">The line to find</param>
+        /// <returns>Null (always)</returns>
         internal override Distance GetDistance(LineFeature line)
         {
             return null;
         }
 
+        /// <summary>
+        /// The features created by this editing operation (may be an empty array, but
+        /// never null).
+        /// </summary>
+        /// <value>An empty array</value>
         internal override Feature[] Features
         {
             get { return new Feature[0]; }
         }
 
+        /// <summary>
+        /// The unique identifier for this edit.
+        /// </summary>
+        /// <value>EditingActionId.Update</value>
         internal override EditingActionId EditId
         {
             get { return EditingActionId.Update; }
@@ -81,15 +115,39 @@ namespace Backsight.Editor.Operations
             return false;
         }
 
+        /// <summary>
+        /// Rollforward this edit in response to some sort of update.
+        /// </summary>
+        /// <param name="uc">The context in which editing revisions are being made (not null).
+        /// Used to hold a record of any positional changes.</param>
+        /// <returns>
+        /// True if operation has been re-executed successfully
+        /// </returns>
         internal override bool Rollforward(UpdateContext uc)
         {
             return true;
         }
 
+        /// <summary>
+        /// Adds references to existing features referenced by this operation (including features
+        /// that are indirectly referenced by observation classes).
+        /// <para/>
+        /// This is called by the <see cref="Complete"/> method, to ensure
+        /// that the referenced features are cross-referenced to the editing operations
+        /// that depend on them.
+        /// </summary>
         public override void AddReferences()
         {
         }
 
+        /// <summary>
+        /// Attempts to locate a superseded (inactive) line that was the parent of
+        /// a specific line.
+        /// </summary>
+        /// <param name="line">The line of interest</param>
+        /// <returns>
+        /// Null (always)
+        /// </returns>
         internal override LineFeature GetPredecessor(LineFeature line)
         {
             return null;
