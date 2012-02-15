e51bc14f90b82dd4966a38df3057598e192a2f16
sstanton <sstanton@btinternet.com>
2012-02-15 22:23:37 +0000

    Avoid display error after import into empty project

--- Backsight.Editor/Forms/NtxImportForm.cs
+++ Backsight.Editor/Forms/NtxImportForm.cs
@@ -132,7 +132,7 @@ namespace Backsight.Editor.Forms
             // If the model is currently empty, we'll want to draw an overview upon
             // completion of the import (otherwise we'll refresh using the currently
             // displayed extent).
-            //bool wasEmpty = CadastralMapModel.Current.IsEmpty;
+            bool wasEmpty = CadastralMapModel.Current.IsEmpty;
 
             ImportOperation i = null;
             EditingController c = EditingController.Current;
@@ -150,13 +150,10 @@ namespace Backsight.Editor.Forms
                 MessageBox.Show(ex.StackTrace, ex.Message);
             }
 
-            // Re-assigning the current model has the desired effect of causing
-            // an overview display...
-            //if (wasEmpty)
-            //    c.MapModel = CadastralMapModel.Current;
-            //else
-            //    c.RefreshAllDisplays();
-            c.RefreshAllDisplays();
+            if (wasEmpty)
+                c.ActiveDisplay.DrawOverview();
+            else
+                c.RefreshAllDisplays();
 
             SaveTranslations();
         }
